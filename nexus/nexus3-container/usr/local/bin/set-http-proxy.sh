ed21fc25a458ec2c50901fa00a43c7d495e53611
Tom Donohue <tom@monodot.co.uk>
2016-10-28 15:32:23 +0100

    Echo in bash script

--- nexus/nexus3-container/usr/local/bin/set-http-proxy.sh
+++ nexus/nexus3-container/usr/local/bin/set-http-proxy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# Sets the web proxy in Nexus if the env var HTTP_PROXY is set
+printf "Detecting HTTP proxy for Nexus...\n\n"
+
+
+# Sets the web proxy in Nexus by parsing the env var HTTP_PROXY
 ${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/set-http-proxy.json
 ${DIR}/nexusscript-run.sh set-http-proxy $HTTP_PROXY
 
