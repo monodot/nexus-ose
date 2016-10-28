ceeedf1b9beec3399bd6e785010eda14f84af124
Tom Donohue <tom@monodot.co.uk>
2016-10-28 16:17:27 +0100

    Adding DIR env var to script

--- nexus/nexus3-container/usr/local/bin/set-http-proxy.sh
+++ nexus/nexus3-container/usr/local/bin/set-http-proxy.sh
@@ -2,6 +2,14 @@
 
 printf "Detecting HTTP proxy for Nexus...\n\n"
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -z "$NEXUS_BASE_URL" ]
+then
+   export NEXUS_BASE_URL="http://localhost:8081"
+fi
+
+$
 
 # Sets the web proxy in Nexus by parsing the env var HTTP_PROXY
 ${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/set-http-proxy.json
