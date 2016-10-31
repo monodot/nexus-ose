c1fd46b4fe60f4cc1538a98275acb402193e9754
Tom Donohue <tom@monodot.co.uk>
2016-10-31 11:30:36 +0000

    Reduced memory requirements for my fragile lil Macbook

--- nexus/nexus3-container/usr/local/bin/set-http-proxy.sh
+++ nexus/nexus3-container/usr/local/bin/set-http-proxy.sh
@@ -9,8 +9,6 @@ then
    export NEXUS_BASE_URL="http://localhost:8081"
 fi
 
-$
-
 # Sets the web proxy in Nexus by parsing the env var HTTP_PROXY
 ${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/set-http-proxy.json
 ${DIR}/nexusscript-run.sh set-http-proxy $HTTP_PROXY
